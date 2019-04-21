64de8463f53da00c95c97a24e4ab36ddafc3cecd
Carter_于 <carteryuwenzheng@163.com>
2019-04-21 13:36:24 +0800

    update MyApplication and AbstractSimpleActivity

--- app/src/main/java/com/carter/androiddemo/MyApplication.kt
+++ app/src/main/java/com/carter/androiddemo/MyApplication.kt
@@ -1,13 +1,32 @@
 package com.carter.androiddemo
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
-class MyApplication : Application{
+class MyApplication : Application
+,HasActivityInjector{
 
     constructor() : super()
 
+    @Inject
+    lateinit var mAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    lateinit var context:Context
+
     override fun onCreate() {
         super.onCreate()
+        context = applicationContext
+        
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity>? {
+        return mAndroidInjector
     }
 
 }
