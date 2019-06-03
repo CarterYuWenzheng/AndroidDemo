6f37b396697d983b3c101d8ec9eae8169bed3825
carter_yu <carteryuwenzheng@163.com>
2019-06-03 16:02:50 +0800

    完善架构

--- app/src/main/java/com/carter/androiddemo/core/data/DataManager.kt
+++ app/src/main/java/com/carter/androiddemo/core/data/DataManager.kt
@@ -2,10 +2,17 @@ package com.carter.androiddemo.core.data
 
 import com.carter.androiddemo.core.db.DBHelper
 import com.carter.androiddemo.core.greenDao.HistoryData
+import com.carter.androiddemo.core.http.BaseResponse
 import com.carter.androiddemo.core.http.HttpHelper
 import com.carter.androiddemo.core.preference.PreferenceHelper
+import com.carter.androiddemo.modules.login.LoginData
+import io.reactivex.Observable
 
 class DataManager(private val httpHelper: HttpHelper, private val dbHelper: DBHelper, private val preferenceHelper: PreferenceHelper) : HttpHelper, DBHelper, PreferenceHelper {
+
+    override fun register(username: String, password: String, rePassWord: String): Observable<BaseResponse<LoginData>> {
+        return httpHelper.register(username, password, rePassWord)
+    }
 
     override fun addHistoryData(data: String): MutableList<HistoryData> {
         return dbHelper.addHistoryData(data)
--- app/src/main/java/com/carter/androiddemo/di/component/BaseFragmentComponent.kt
+++ app/src/main/java/com/carter/androiddemo/di/component/BaseFragmentComponent.kt
@@ -1,4 +1,17 @@
 package com.carter.androiddemo.di.component
 
-interface BaseFragmentComponent {
+import com.carter.androiddemo.base.fragment.BaseFragment
+import com.carter.androiddemo.base.presenter.IPresenter
+import com.carter.androiddemo.base.view.IView
+import dagger.Subcomponent
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+
+@Subcomponent(modules = [AndroidInjectionModule::class])
+interface BaseFragmentComponent : AndroidInjector<BaseFragment<IPresenter<IView>>> {
+
+    @Subcomponent.Builder
+    abstract class BaseBuilder : AndroidInjector.Builder<BaseFragment<IPresenter<IView>>>() {
+
+    }
 }
